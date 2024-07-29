5c6ac32fbc1f2e35125a76a1cd4624af0cd70855
Jeronimo Cabezuelo <jeronimocabezuelo@gmail.com>
2024-07-29 17:20:09 +0200

    Initial Commit

---
+++ InfoEnergy/InfoEnergyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoEnergyApp.swift
+//  InfoEnergy
+//
+//  Created by Jerónimo Cabezuelo Ruiz on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InfoEnergyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
